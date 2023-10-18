c768d9fa56f10274e5e999986b764cdafc7daf81
hakingdev <j72Y7c62Nm@2fQJ>
2023-10-18 13:21:08 +0200

    Home_work_17_10_2023

--- src/Homework6_17_10_2023.java
+++ src/Homework6_17_10_2023.java
@@ -1,10 +1,14 @@
+import student.Game;
 import student.WeddingYear;
 
 import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Homework6_17_10_2023 {
     public static void main(String[] args) {
+
+        //task 1
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сколько лет вы в браке: ");
         int year = scanner.nextInt();
@@ -23,5 +27,35 @@ public class Homework6_17_10_2023 {
             default -> throw new RuntimeException("Не знаю такой :)");
         };
         System.out.println(yearsWedding);
+        game();
+    }
+
+    //task 2
+    public static void game() {
+        Scanner scanner = new Scanner(System.in);
+        Random rnd = new Random();
+        System.out.println("Введите, что выбираете: \"Камень\", \"Ножницы\" или \"Бумага\" ");
+        String choiсe = scanner.nextLine();
+        Game noDefault = Game.valueOf(choiсe.toUpperCase());
+        Game userChoice = switch (noDefault) {
+            case КАМЕНЬ -> Game.КАМЕНЬ;
+            case НОЖНИЦЫ -> Game.НОЖНИЦЫ;
+            case БУМАГА -> Game.БУМАГА;
+        };
+        System.out.println("Твой выбор " + userChoice);
+        System.out.println("Теперь бросает выбор компьютер");
+        int program = rnd.nextInt(Game.values().length);
+        Game progChoice = Game.values()[program];
+        System.out.println("Компьютер выбрал " + progChoice);
+        if (userChoice.equals(progChoice)) {
+            System.out.println("Ничья");
+        } else if (userChoice == Game.НОЖНИЦЫ && progChoice == Game.БУМАГА
+                || userChoice == Game.КАМЕНЬ && progChoice == Game.НОЖНИЦЫ
+                || userChoice == Game.БУМАГА && progChoice == Game.КАМЕНЬ) {
+            System.out.println("Вы победили");
+
+        } else {
+            System.out.println("Компьютер победил");
+        }
     }
 }
