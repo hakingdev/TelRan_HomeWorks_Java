18b2eaa11eff186245d27970a348207ca4a3adb5
hakingdev <145497490+hakingdev@users.noreply.github.com>
2023-10-24 14:16:50 +0200

    Add files via upload
    19.10.2023 HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,74 @@
+import week.DayOfWeek;
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        String trianlge = "";
-        trianlge = (a == b) && (b == c) && (c == a) ? "равносторонний" :
-                (a == b || b == c || c == a) ? "равнобедренный" :
-                        "разносторонний";
-        System.out.println(trianlge);
-        pointSystem();
-        moneyConvertetion();
+        /*
+
+        Напиши программу, которая моделирует ситуацию.
+        Ты попросил(а) друзей скинуться на подарок на твой День Рождения. Каждый друг случайным образом может подарить
+        тебе одну купюру номиналом 50, 100, 200 или 500 долларов. Твоя цель - новенький игровой компьютер, который стоит 10 000 долларов.
+        Как только друзья подарят тебе нужную сумму (или даже чуть больше),
+        останавливай сбор подарков и веди всех выпить за твоё здоровье в лучший бар города!
+
+         */
+        //task 1
+        Random rnd = new Random();
+        int fullAmount = 10000;
+        int current = 0;
+        do {
+            int random = rnd.nextInt(5);
+            int money = switch (random) {
+                case 1 -> 50;
+                case 2 -> 100;
+                case 3 -> 200;
+                case 4 -> 500;
+                default -> 0;
+            };
+            current += money;
+        } while (current <= fullAmount);
+        int rest = 0;
+        rest = current - fullAmount;
+        System.out.printf("Спасибо, на подарок я собрали всю сумму %d$.%nПриглашаю всех в бар на остаток от суммы %d$, отметить!%n", current, rest);
+
+        summNumber();
+        massiveFirst();
     }
 
-    public static void pointSystem() {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        if (a >= 80) {
-            System.out.println("A");
-        } else if (a <= 79 && a >= 60) {
-            System.out.println("B");
-        } else if (a >= 40) {
-            System.out.println("С");
-        } else if (a <= 39 && a >= 20) {
-            System.out.println("D");
-        } else {
-            System.out.println("F");
-        }
+    //task 2
+    public static void summNumber() {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        do {
+            System.out.println("Введите число по которому будем считать сумму нечетных чисел: ");
+            int number = sc.nextInt();
+            for (int i = 0; i < number; i++) {
+                if (i % 2 != 0) {
+                    sum += i;
+                }
+            }
+            System.out.println("Сумма всех нечетных чисел " + sum);
+            System.out.println("Введите \"quit\" если хотите закончить");
+            String quit = sc.nextLine();
+            String quit2 = sc.nextLine();
+            if (quit2.equalsIgnoreCase("quit")) {
+                return;
+            }
+        } while (true);
     }
 
-    public static void moneyConvertetion() {
-        System.out.println("Выберите валюту для конвертации: usd, cny, uah ");
-        double usd, cny, uah;
-        Scanner scanner = new Scanner(System.in);
-        String converts = scanner.nextLine();
-        System.out.println("Введите количество: ");
-        float amount = scanner.nextFloat();
-        switch (converts) {
-            case "usd" -> {
-                usd = amount * 1.05;
-                System.out.println(amount + " Euro = " + usd + " Dollars ");
-            }
-            case "cny" -> {
-                cny = amount * 7.64;
-                System.out.println(amount + " Euro = " + cny + " Youan ");
-            }
-            case "uah" -> {
-                uah = amount * 7.64;
-                System.out.println(amount + " Euro = " + uah + " Gryven ");
-            }
-            default -> System.out.println("Error");
+
+    public static void massiveFirst() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите текущий день недели: ");
+        String userInput = sc.nextLine();
+        DayOfWeek[] dayOfWeek = DayOfWeek.values();
+        for (DayOfWeek week:dayOfWeek) {
+            if (!week.name().equals(userInput))
+                System.out.println(week);
         }
     }
 }
+
